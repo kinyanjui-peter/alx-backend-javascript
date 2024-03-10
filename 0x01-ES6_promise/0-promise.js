14e21fac001db33cac4a88db1413dc8f904de574
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-10 14:06:24 +0300

    returns a promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,9 @@
 //Return a Promise using this prototype function getResponseFromAPI()
+
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('True');
-        }, 2000);
-    });
+  return new Promise((resolve,) => {
+    setTimeout(() => {
+      resolve('True');
+    }, 2000);
+  });
 }
