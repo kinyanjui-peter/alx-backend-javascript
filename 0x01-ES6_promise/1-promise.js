faf2e5e68155e92a5cb60e229c1bdb25b783659b
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-10 15:05:19 +0300

    use promise, if you can keep them

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,18 @@
+//  return a promise with a boolean parameter
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+     // const trueorfalse = true;
+      if (success) {
+        const respdata = {
+          status: '200',
+          body: 'Success'
+        };
+        resolve(respdata);
+      } else {
+        reject('The fake API is not working currently');
+      }
+    }, 2000);
+  });
+}
