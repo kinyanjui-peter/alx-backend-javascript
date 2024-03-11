2c2debc172a0156537cd44cc8ff8ddf12a8d1476
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-11 22:53:48 +0300

    import class

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -3,20 +3,21 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-        throw new Error('name must be a string')
+      throw new Error('name must be a string');
       }
-      if (typeof length !== 'number') {
-        throw new Error('length must be a number')
-      }
-      if (!Array.isArray(students)) {
-        throw new Error('students must be an Array')
-      }
+    if (typeof length !== 'number') {
+      throw new Error('length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new Error('students must be an Array');
+    }
 
     this._name = name;
     this._length = length;
     this._students = students;
   }
   // get to get name
+  
   get name() {
     return this._name;
   }
@@ -25,22 +26,22 @@ export default class HolbertonCourse {
   set name(value) {
     this._name = value;
   }
- 
+
   // get to get length
   get length() {
     return this._length;
   }
- 
+
   // setter to update length
   set length(value) {
     this._length = value;
   }
- 
+
   // get to get students
   get students() {
     return this._students;
   }
- 
+
   // setter to update students
   set students(value) {
     this._students = value;
