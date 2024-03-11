c1f7cc201c837e1183b2146894290d6788d7ae8a
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-11 20:29:17 +0300

    class implementation

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,6 +2,17 @@
 
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    let _maxStudentsSize = this.maxStudentsSize
+    // asign parameter to private variable
+    this._maxStudentsSize = maxStudentsSize;
   }
+
+    // Getter method to access maximum student size
+    getmaxStudentsSize() {
+        return this._maxStudentsSize;
+    };
+
+    // setter method to set the value of maxStudentsSize
+    setmaxStudentsSize(newSize) {
+        return this._maxStudentsSize = newSize;
+    }
 }
