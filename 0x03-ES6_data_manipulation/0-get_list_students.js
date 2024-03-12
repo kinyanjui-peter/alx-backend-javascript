56e46cd9500d7df34024c311cb488e1b5a763597
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-12 22:00:12 +0300

    object list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+// function that returns an array object
+
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ]
+}
