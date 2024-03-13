fc2cc404b9f1b4d3a38e0b61d2d1d36ff21897a8
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-13 07:46:03 +0300

    reducer

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,9 +1,8 @@
 export default function getStudentIdsSum(getListStudents) {
 
   const total = getListStudents.reduce(gettotal, 0)
-  console.log(`${total}`)
-
   function gettotal(previous, next) {
     return previous + next.id;
   }
+  console.log(`${total}`)
 }
