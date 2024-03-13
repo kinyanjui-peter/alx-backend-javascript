f04f280a1a22064834c4e7c318a1adac382ec365
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-13 10:59:08 +0300

    filter nad map

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,21 @@
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  if (!Array.isArray(newGrades)) {
+    console.log('new grades must be an array')
+    return;
+  }
+  if (typeof city !== 'string') {
+      console.log('city must be a string')
+      return;
+  }
+
+  // filter and return student in a given city.
+  const filteredcity = getListStudents().filter(student => student.location === city);
+
+  const updatedstudent = filteredcity.map(student => ({...student, grade:newGrades}));
+
+  updatedstudent.forEach(student => {
+    if (updatedstudent.grade === null) {
+      student.grade = ('N/A')
+  0      }})
+  return updatedstudent;
+}
