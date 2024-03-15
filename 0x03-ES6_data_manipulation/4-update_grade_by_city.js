78c0a455f72bfef990ec1132c29c98114f3d0423
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-15 21:32:33 +0300

    combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -9,12 +9,13 @@ export default function updateStudentGradeByCity(getListStudents, city, newGrade
   }
 
   // filter and return student in a given city.
-  const filteredcity = getListStudents().filter(student => student.location === city);
-  const updatedstudent = filteredcity.map(student => ({...student, grade:newGrades}));
+  const filteredcity = getListStudents.filter(student =>
+    student.location === city);
+  const updatedstudent = filteredcity.map((student, index) => ({...student,
+     grade: newGrades[index] !== undefined ? newGrades[index] : 'N/A',}));
 
-  updatedstudent.forEach(student => {
-    if (updatedstudent.grade === null) {
-      student.grade = ('N/A')
-  0      }})
+  // updatedstudent.forEach(student => {
+  //   if (updatedstudent.grade === null) {
+  //     student.newGrades.grade = ('N/A')
   return updatedstudent;
 }
