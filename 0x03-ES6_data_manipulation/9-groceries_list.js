66925c82ab6e7afcd91d0d580ce14fb5bb886333
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-15 22:52:41 +0300

    Map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
-function groceriesList() {
-    const groceriesMap = new Map([
-      ['Apples', 10],
-      ['Tomatoes', 10],
-      ['Pasta', 1],
-      ['Rice', 1],
-      ['Banana', 5]
-    ]);
-    return groceriesMap;
-  }
+export default function groceriesList() {
+  const groceriesMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+]);
+return groceriesMap;
+}
