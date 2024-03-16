d2b6221baec0f6c9da38c34bc63fd33d97968fd9
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-16 19:46:48 +0300

    Promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,20 +1,20 @@
 export default function handleResponseFromAPI(promise) {
-	return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       try {
         if (!promise) {
-					throw new Error('');
+          throw new Error('');
         }
         const resolvedpromise = {
           status: '200',
           body: 'success',
-				};
+        };
         resolve(resolvedpromise);
-			} catch (error) {
-				reject(error);
-        } finally {
-					console.log('Got a response from the API');
-				}
+      } catch (error) {
+        reject(error);
+      } finally {
+        console.log('Got a response from the API');
+      }
     }, 2000);
-	});
+  });
 }
