7a67613642e0b5313f29725067b99d7f48f383cc
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-16 09:50:58 +0300

    The wonders of spread syntax

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-    const stringarr = [...string];
-    return [...array1, ...array2, ...stringarr];
+  const stringarr = [...string];
+  return [...array1, ...array2, ...stringarr];
 }
