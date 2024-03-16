51fe3bbecb811d6ceb5311144e1c587095531f3a
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-16 23:35:40 +0300

    Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,31 +1,12 @@
-import { uploadPhoto } from "./utils"
-import { createUser } from "./utils"
+import { uploadPhoto, createUser } from "./utils.js";
 
-// export default function handleProfileSignup() {
-//   return new Promise((resolve, reject) => {
-//     try {
-//       setTimeout(() =>{
-//         const photo = {
-//           body: uploadPhoto(),
-//           firstName: 'Peter',
-//           lastName: 'Kinyanjui',
-//         }
-//         resolve(photo);
-//         } else {
-//         reject('Signup system offline');
-//     }, 2000)
-//   } 
-//   )
-// }
 async function handleProfileSignup() {
-  try {
-      const photoResponse = await uploadPhoto();
-      const userResponse = await createUser();
-      
-      console.log(`${photoResponse.body.firstName} ${userResponse.body.lastName}`);
-  } catch (error) {
-      console.error("Signup system offline");
-  }
+    try {
+        const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+        console.log(`${photoResponse} ${userResponse.body.firstName} ${userResponse.body.lastName}`);
+    } catch (error) {
+        console.error("Signup system offline");
+    }
 }
 
 export default handleProfileSignup;
