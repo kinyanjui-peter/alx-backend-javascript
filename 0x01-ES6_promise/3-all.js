066a70d5f8ea836e9656f3d9b44282cbcb2d6ea4
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-16 23:20:18 +0300

    Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,31 @@
 import { uploadPhoto } from "/.utils"
 import { createUser } from "./utils"
 
-export default function handleProfileSignup() {
-  return new Promise((resolve) => {
-    setTimeout(() =>{
-      uploadPhoto()
-      resolve(createUser);
-    }, 2000)
-  } else {
-    reject('Signup system offline');
+// export default function handleProfileSignup() {
+//   return new Promise((resolve, reject) => {
+//     try {
+//       setTimeout(() =>{
+//         const photo = {
+//           body: uploadPhoto(),
+//           firstName: 'Peter',
+//           lastName: 'Kinyanjui',
+//         }
+//         resolve(photo);
+//         } else {
+//         reject('Signup system offline');
+//     }, 2000)
+//   } 
+//   )
+// }
+async function handleProfileSignup() {
+  try {
+      const photoResponse = await uploadPhoto();
+      const userResponse = await createUser();
+      
+      console.log(`${photoResponse.body.firstName} ${userResponse.body.lastName}`);
+  } catch (error) {
+      console.error("Signup system offline");
   }
-  )
 }
+
+export default handleProfileSignup;
